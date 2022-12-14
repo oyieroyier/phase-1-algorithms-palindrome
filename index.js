870a86584839279a87dba8d1e3d6eff09a2bec11
Bob Oyier <boboyier92@gmail.com>
2022-12-14 08:44:00 +0300

    "Completed the Palindrome lab"

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+	// Write your algorithm here
+	const arrayOfLetters = word.split("");
+	const reverseLetters = arrayOfLetters.reverse();
+	const reverseString = reverseLetters.join("");
+
+	if (word == reverseString) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /* 
   Add your pseudocode here
-*/
 
-/*
+  Split the array of letter to single letter
+  check if word is equal to its reversed version
+  Join the reversed version to an array or a word
+    if word is the same as its reversed version:
+        return true
+    if word is not the same as its reversed version
+        return false
+
   Add written explanation of your solution here
+/*
+  first: Split the array of letter to single letter
+  second: convert the received word into its reversed version
+  third: Join the reversed version to an array or a word
+  fourth: check if the received word and the reversed version are the same
+  lastly: return true or false accordingly
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+	console.log("Expecting: false");
+	console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
